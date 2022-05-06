cee124995fe108e010631cec2504aac23a758dd6
BulbyVR <26726264+TheDrawingCoder-Gamer@users.noreply.github.com>
2022-05-06 12:30:45 -0400

    Trying my best

--- ffi/ffi.cpp
+++ ffi/ffi.cpp
@@ -1,74 +1,91 @@
+
 #include "ffi.h"
-#include "device.hh"
-#include <pulse/pulseaudio.h>
-#include "pulseaudio.hh"
-#include <string>
-#include <list>
-#include <array>
-#include <vector>
-#include <algorithm>
+
+template <typename D, typename T>
+std::list<D*> magic_list(std::list<T> list) {
+    auto ret = new std::list<D*>(list.size());
+    for (auto elem : list) {
+        ret->push_back(reinterpret_cast<D*>(&elem));
+    }
+    return *ret;
+}
+template <typename T>
+harray_t* to_c_array(std::list<T> list) {
+    std::vector<T>* vector = new std::vector(std::begin(list), std::end(list));
+    harray_t* arr = (harray_t*) malloc(sizeof(harray_t));
+    arr->size = vector->size();
+    arr->arr = vector->data();
+    return arr;
+}
+
 extern "C" {
+   
+
     ffi_pulseaudio* pulseaudio_create(const char* name) {
         auto object = new Pulseaudio(std::string(name));
-	return reinterpret_cast<ffi_pulseaudio*>(object);
+	return reinterpret_cast< ffi_pulseaudio* >(object);
     }
     void pulseaudio_destroy(ffi_pulseaudio* obj) {
-        auto pulse = reinterpret_cast<Pulseaudio*>(obj);
+        auto pulse = reinterpret_cast< Pulseaudio* >(obj);
 	delete pulse;
     }
 
-    haskell_array* pulseaudio_get_sinks(ffi_pulseaudio* obj) {
-        auto pulse = reinterpret_cast<Pulseaudio*>(obj);
-        return to_c_array(reinterpret_cast<std::list<ffi_pulseaudio*>(pulse->get_sinks()));
+    harray_t* pulseaudio_get_sinks(ffi_pulseaudio* obj) {
+        auto pulse = reinterpret_cast< Pulseaudio* >(obj);
+        auto sinks = pulse->get_sinks();
+
+        return to_c_array(magic_list<ffi_device>(sinks));
     }
-    haskell_array* pulseaudio_get_sources(ffi_pulseaudio* obj) {
-        auto pulse = reinterpret_cast<Pulseaudio*>(obj);
-        return to_c_array(reinterpret_cast<std::list<ffi_pulseaudio*>((pulse->get_sources())&));
+    harray_t* pulseaudio_get_sources(ffi_pulseaudio* obj) {
+        auto pulse = reinterpret_cast< Pulseaudio* >(obj);
+        return to_c_array(magic_list<ffi_device>(pulse->get_sources()));
     }
     ffi_device* pulseaudio_get_sink_index(ffi_pulseaudio* obj, uint32_t index) {
-        auto pulse = reinterpret_cast<Pulseaudio*>(obj);
-	return reinterpret_cast<ffi_device*>(obj->get_sink(index));
+        auto pulse = reinterpret_cast< Pulseaudio* >(obj);
+        auto sink = pulse->get_sink(index);
+	return reinterpret_cast<ffi_device*>(&sink);
     }
     ffi_device* pulseaudio_get_sink_name(ffi_pulseaudio* obj, const char* name) {
-        auto pulse = reinterpret_cast<Pulseaudio*>(obj);
-	return reinterpret_cast<ffi_device*>(obj->get_sink(std::string(name)));
+        auto pulse = reinterpret_cast< Pulseaudio* >(obj);
+	return reinterpret_cast<ffi_device*>(pulse->get_sink(std::string(name)));
     }
     ffi_device* pulseaudio_get_source_index(ffi_pulseaudio* obj, uint32_t index) {
-        auto pulse = reinterpret_cast<Pulseaudio*>(obj);
-	return reinterpret_cast<ffi_device*>(obj->get_source(index));
+        auto pulse = reinterpret_cast<Pulseaudio *>(obj);
+	return reinterpret_cast<ffi_device *>(pulse->get_source(index));
     }
     ffi_device* pulseaudio_get_source_name(ffi_pulseaudio* obj, const char* name) {
-        auto pulse = reinterpret_cast<Pulseaudio*>(obj);
-	return reinterpret_cast<ffi_device*>(obj->get_source(std::string(name)));
+        auto pulse = reinterpret_cast<Pulseaudio *>(obj);
+        auto source = pulse->get_source(std::string(name));
+	return reinterpret_cast<ffi_device *>(&source);
     }
     void pulseaudio_set_volume(ffi_pulseaudio* obj, ffi_device* device, pa_volume_t new_volume) {
-        auto pulse = reinterpret_cast<Pulseaudio*>(obj);
+        auto pulse = reinterpret_cast<Pulseaudio *>(obj);
 	pulse->set_volume(reinterpret_cast<Device&>(device), new_volume);
     }
     void pulseaudio_set_mute(ffi_pulseaudio* obj, ffi_device* device, bool mute) {
-        auto pulse = reinterpret_cast<Pulseaudio*>(obj);
+        auto pulse = reinterpret_cast<Pulseaudio *>(obj);
 	pulse->set_mute(reinterpret_cast<Device&>(device), mute);
     }
     uint32_t device_index(ffi_device* device) {
-        return reinterpret_cast<Device*>(device)->index;
+        return reinterpret_cast<Device *>(device)->index;
     }
     int device_type(ffi_device* device) {
-        return reinterpret_cast<Device*>(device)->type;
+        return reinterpret_cast<Device* >(device)->type;
     }
     const char* device_name(ffi_device* device) {
-        return reinterpret_cast<Device*>(device)->name.c_str();
+        return reinterpret_cast<Device* >(device)->name.c_str();
     }
     const char* device_description(ffi_device* device) {
-        return reinterpret_cast<Device*>(device)->description.c_str();
+        return reinterpret_cast<Device* >(device)->description.c_str();
     }
     int device_state(ffi_device* device) {
-        return reinterpret_cast<Device*>(device)->state;
+        return reinterpret_cast<Device *>(device)->state;
     }
     pa_cvolume device_volume(ffi_device* device) {
-        return reinterpret_cast<Device*>(device)->volume;
+        return reinterpret_cast<Device *>(device)->volume;
     }
     pa_volume_t device_volume_avg(ffi_device* device) {
-        return reinterpret_cast<Device*>(device)->volume_avg;
+        return reinterpret_cast<Device *>(device)->volume_avg;
     }
 
     int device_volume_percent(ffi_device* device) {
@@ -77,16 +94,16 @@ extern "C" {
     bool device_mute(ffi_device* device) {
         return reinterpret_cast<Device*>(device)->mute;
     }
-
+    /*
     ffi_device* device_create_source(const pa_source_info* info) {
-	return reinterpret_cast<ffi_device*>(new Device(info));
+        return reinterpret_cast<ffi_device*>(new Device(info));
     }
     ffi_device* device_create_sink(const pa_sink_info* info) {
         return reinterpret_cast<ffi_device*>(new Device(info));
     }
-
+    */
     ffi_server_info* server_create() {
-        return reinterpet_cast<ffi_server_info*>(new ServerInfo());
+        return reinterpret_cast<ffi_server_info*>(new ServerInfo());
     }
     void server_destroy(ffi_server_info* info) {
 	auto server = reinterpret_cast<ServerInfo*>(info);
@@ -103,19 +120,11 @@ extern "C" {
 	auto obj = reinterpret_cast<Device*>(device);
 	delete device;
     }
-    uint32_t harray_size(haskell_array* arr) {
+    uint32_t harray_size(harray_t* arr) {
         return arr->size;
     }
-    void* harray_array(haskell_array* arr) {
+    void* harray_array(harray_t* arr) {
         return arr->arr;
     }
-haskell_array* to_c_array(std::list<void*> list) {
-    std::vector<void*> vector = new std::vector(list.size());
-    std::copy(list.begin(), list.end(), vector);
-    struct haskell_array* arr = (struct haskell_array*) malloc(sizeof(struct haskell_array));
-    arr->size = vector.size();
-    arr->arr = vector.data();
-    return arr;
-}
     
 }
--- ffi/ffi.h
+++ ffi/ffi.h
@@ -1,8 +1,24 @@
+#ifndef FFI_H 
+#define FFI_H
+#include "device.hh"
 #include <pulse/pulseaudio.h>
+#include <string>
+#include <list>
+#include <array>
+#include <vector>
+#include <algorithm>
+#include "pulseaudio.hh"
+#ifdef __cplusplus
 extern "C" {
-    struct ffi_server_info; 
-    struct ffi_device;
-    struct ffi_pulseaudio;
+#endif
+    struct haskell_array {
+        uint32_t size;
+        void* arr;
+    };
+    typedef struct haskell_array harray_t;
+    typedef struct ffi_server_stct ffi_server_info; 
+    typedef struct ffi_device_stct ffi_device;
+    typedef struct ffi_pulseaudio_stct ffi_pulseaudio;
     ffi_server_info* server_create();
     void server_destroy(ffi_server_info* object);
     const char* server_default_source_name(ffi_server_info* object);
@@ -11,8 +27,8 @@ extern "C" {
     int pulseaudio_state(ffi_pulseaudio* object);
     ffi_pulseaudio* pulseaudio_create(const char* name);
     void pulseaudio_destroy(ffi_pulseaudio* object);
-    ffi_device** pulseaudio_get_sinks(ffi_pulseaudio* object);
-    ffi_device** pulseaudio_get_sources(ffi_pulseaudio* object);
+    harray_t* pulseaudio_get_sinks(ffi_pulseaudio* object);
+    harray_t* pulseaudio_get_sources(ffi_pulseaudio* object);
     ffi_device* pulseaudio_get_sink_index(ffi_pulseaudio* obj, uint32_t index);
     ffi_device* pulseaudio_get_sink_name(ffi_pulseaudio* obj, const char* name);
     ffi_device* pulseaudio_get_source_index(ffi_pulseaudio* obj, uint32_t index);
@@ -29,16 +45,15 @@ extern "C" {
     pa_volume_t device_volume_avg(ffi_device* obj);
     int device_volume_percent(ffi_device* obj);
     bool device_mute(ffi_device* obj);
+    /*
     ffi_device* device_create_source(const pa_source_info* i);
     ffi_device* device_create_sink(const pa_sink_info* i);
+    */
     void device_destroy(ffi_device* obj);
     bool device_mute(ffi_device* obj);
-
-    struct haskell_array {
-        uint32_t size;
-        void* arr;
-    }
-
-    uint32_t harray_size(haskell_array* arr);
-    void* harray_array(haskell_array* arr);
+    uint32_t harray_size(harray_t arr);
+    void* harray_array(harray_t* arr);
+#ifdef __cplusplus
 }
+#endif
+#endif // FFI_H
